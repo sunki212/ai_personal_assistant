0764412abe9e7a6896f263c953a62a61e134da51
Valeria <sunny.kirina@inbox.ru>
2025-07-16 20:04:27 +0300

    Delete config/config.py

--- config/config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = "" # Вставьте свой токен
-ADMIN_GROUP_ID = -100  # ID вашей группы в ТГ с админами
-DEESEEK_API_URL = "https://api.deepseek.com/v1/chat/completions"
-DEESEEK_API_KEY1 = "" # Вставьте свой API ключ
-
-developers = [] # Вставьте ID разработчиков
-owners = {} # Вставьте ID владельцев
